bd29e8f017b1bb65815ef5be4cd3a70af30fdd49
Robin Yeh <robin.yeh@broadcom.com>
2021-10-05 21:47:00 +0800

    enable cors

--- src/main/java/com/example/macro/controller/MacroController.java
+++ src/main/java/com/example/macro/controller/MacroController.java
@@ -4,6 +4,7 @@ import com.example.macro.model.Country;
 import com.example.macro.repository.CountryRepository;
 import com.example.macro.service.MacroDataPopulatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.example.macro.service.MacroService;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class MacroController {
 
