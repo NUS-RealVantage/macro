781af8013cd8d2b0692e0d678750a026754dfa20
Erwin Lee <erwin@mokapos.com>
2021-10-25 10:18:09 +0800

    update run.sh script

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-nohup java -jar /home/ubuntu/realvantage/macro/macro-0.0.1-SNAPSHOT.jar &
+fuser -k 8765/tcp
+nohup java -Dspring.config.location=/home/ubuntu/realvantage/macro/application.properties -jar /home/ubuntu/realvantage/macro/macro-0.0.1-SNAPSHOT.jar &
